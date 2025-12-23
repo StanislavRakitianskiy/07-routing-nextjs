943fad851c99b66f3a9880228be8009e520a3f08
StanislavRakitianskiy <raketaswig@gmail.com>
2025-12-23 19:46:09 +0200

    fix files

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -2,28 +2,33 @@
 
 import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
-
 import Modal from "@/components/Modal/Modal";
 import Loader from "@/components/Loader/Loader";
 import ErrorMessage from "@/components/ErrorMessage/ErrorMessage";
 import { fetchNoteById } from "@/lib/api";
-
 import css from "./NotePreview.module.css";
 
 export default function NotePreviewClient({ id }: { id: string }) {
   const router = useRouter();
 
-  const { data: note, isLoading, isError, error } = useQuery({
+  const {
+    data: note,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     enabled: Boolean(id),
+    refetchOnMount: false,
   });
 
   return (
     <Modal onClose={() => router.back()}>
       {isLoading && <Loader />}
-
-      {isError && <ErrorMessage message={(error as Error)?.message || "Error"} />}
+      {isError && (
+        <ErrorMessage message={(error as Error)?.message || "Error"} />
+      )}
 
       {note && (
         <div className={css.container}>
